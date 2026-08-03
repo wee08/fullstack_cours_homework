99150031c0df47e22de946370b53df14f8723fc0
wee08 <manutnithyasean@gmail.com>
2026-08-03 10:25:00 +0700

    init index.js file

---
+++ product_management/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("homepage");
+});
+
+app.listen(PORT, () => {
+  console.log(`server is running on http://localhost:${PORT}`);
+});
