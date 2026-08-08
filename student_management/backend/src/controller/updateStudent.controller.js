b235788e99573cb1b0f16d4df75bfa3d29bbf162
wee08 <manutnithyasean@gmail.com>
2026-08-08 11:47:14 +0700

    feat:update student base on their id

---
+++ student_management/backend/src/controller/updateStudent.controller.js
@@ -0,0 +1,38 @@
+const db = require("../config/config");
+const logs_error = require("../helper/logs_error");
+const udpateStudent = async (req, res) => {
+  try {
+    const sql = `
+        UPDATE students SET id=?,name=?,gender=?,std_class=?,phone=?,image_url=?,remark=? 
+        WHERE students.id=?        
+    `;
+    const targetId = req.params.targetId;
+    const field = ({ id, name, gender, std_class, phone, image_url, remark } =
+      req.body);
+
+    await db.query(sql, [
+      id,
+      name,
+      gender,
+      std_class,
+      phone,
+      image_url,
+      gender,
+      targetId,
+    ]);
+
+    res.send({
+      status: true,
+      message: "updated student!",
+    });
+  } catch (error) {
+    const content = error.message + "\n";
+    logs_error(content);
+    res.status(500).send({
+      status: false,
+      message: content,
+    });
+  }
+};
+
+module.exports = udpateStudent;
--- student_management/backend/src/router/student_managment.route.js
+++ student_management/backend/src/router/student_managment.route.js
@@ -1,9 +1,11 @@
 const createStudent = require("../controller/createStudent.controller");
 const getAllStudents = require("../controller/getAllStudents.controller");
+const udpateStudent = require("../controller/updateStudent.controller");
 
 const student_managementRoute = (app) => {
   app.get("/api/v1/student/get/all", getAllStudents);
   app.post("/api/v1/student/create", createStudent);
+  app.put("/api/v1/student/update/:targetId", udpateStudent);
 };
 
 module.exports = student_managementRoute;
