0bce3b59dab68c641b065cc5144ef97e81a12cbf
wee08 <manutnithyasean@gmail.com>
2026-08-08 13:50:11 +0700

    feat: add token_validatoin to all route

--- student_management/backend/src/router/student_managment.route.js
+++ student_management/backend/src/router/student_managment.route.js
@@ -5,9 +5,13 @@ const deleteStudent = require("../controller/deleteStudent.controller");
 const validate_token = require("../middleware/auth");
 const student_managementRoute = (app) => {
   app.get("/api/v1/student/get/all", validate_token(), getAllStudents);
-  app.post("/api/v1/student/create", createStudent);
-  app.put("/api/v1/student/update/:targetId", udpateStudent);
-  app.delete("/api/v1/student/delete/:targetId", deleteStudent);
+  app.post("/api/v1/student/create", validate_token(), createStudent);
+  app.put("/api/v1/student/update/:targetId", validate_token(), udpateStudent);
+  app.delete(
+    "/api/v1/student/delete/:targetId",
+    validate_token(),
+    deleteStudent,
+  );
 };
 
 module.exports = student_managementRoute;
