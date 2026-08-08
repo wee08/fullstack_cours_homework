72baeac1a604d1515bb1cb03050d5689413985d5
wee08 <manutnithyasean@gmail.com>
2026-08-08 14:28:28 +0700

    fix:turn host and user to secret for practice

--- student_management/backend/src/config/config.js
+++ student_management/backend/src/config/config.js
@@ -1,7 +1,14 @@
 const mysql = require("mysql2/promise");
+const path = require("path");
+const dotenv = require("dotenv");
+dotenv.config({ path: path.resolve(__dirname, "../../../.env") });
+
+const STUDNET_DB_HOST = process.env.STUDNET_DB_HOST;
+const STUDNET_DB_USER = process.env.STUDNET_DB_USER;
+
 const studentDB = mysql.createPool({
-  host: "localhost",
-  user: "root",
+  host: STUDNET_DB_HOST,
+  user: STUDNET_DB_USER,
   password: "",
   database: "student_management",
 });
