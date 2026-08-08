ee1f21a1cd2d6d44bb054c2e9f9238f4f4d68f95
wee08 <manutnithyasean@gmail.com>
2026-08-08 12:16:03 +0700

    feat: delete student base on id

---
+++ student_management/backend/src/controller/deleteStudent.controller.js
@@ -0,0 +1,26 @@
+const db = require("../config/config");
+const logs_error = require("../helper/logs_error");
+const { checkTargetId } = require("../helper/validate");
+
+const deleteStudent = async (req, res) => {
+  try {
+    const sql = `DELETE FROM students WHERE students.id=?`;
+    const targetId = req.params.targetId;
+    await checkTargetId(res, targetId);
+
+    await db.query(sql, [targetId]);
+    res.send({
+      status: true,
+      message: "Deleted student!",
+    });
+  } catch (error) {
+    const content = error.message + "\n";
+    logs_error(content);
+    res.status(500).send({
+      status: false,
+      message: content,
+    });
+  }
+};
+
+module.exports = deleteStudent;
--- student_management/backend/src/router/student_managment.route.js
+++ student_management/backend/src/router/student_managment.route.js
@@ -1,11 +1,12 @@
 const createStudent = require("../controller/createStudent.controller");
 const getAllStudents = require("../controller/getAllStudents.controller");
 const udpateStudent = require("../controller/updateStudent.controller");
-
+const deleteStudent = require("../controller/deleteStudent.controller");
 const student_managementRoute = (app) => {
   app.get("/api/v1/student/get/all", getAllStudents);
   app.post("/api/v1/student/create", createStudent);
   app.put("/api/v1/student/update/:targetId", udpateStudent);
+  app.delete("/api/v1/student/delete/:targetId", deleteStudent);
 };
 
 module.exports = student_managementRoute;
