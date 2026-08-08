ba3f505451324c8b056c65965627c2355db06d15
wee08 <manutnithyasean@gmail.com>
2026-08-08 16:15:53 +0700

    fix: mysql error

--- student_management/backend/src/helper/fetchAllStudents.js
+++ student_management/backend/src/helper/fetchAllStudents.js
@@ -1,7 +1,7 @@
 const { studentDB } = require("../config/config");
 
 async function fetchAllStudent(table) {
-  const sql = `SELECT * FROM ?`;
+  const sql = `SELECT * FROM ??`;
   const result = await studentDB.query(sql, [table]);
   return result[0];
 }
