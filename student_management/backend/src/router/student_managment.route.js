c2f1bf72b204d9053a8eb2670790f87a96a4dd3c
wee08 <manutnithyasean@gmail.com>
2026-08-09 10:56:08 +0700

    install telegram bot api

--- student_management/backend/src/router/student_managment.route.js
+++ student_management/backend/src/router/student_managment.route.js
@@ -6,10 +6,10 @@ const deleteStudent = require("../controller/student/deleteStudent.controller");
 const validate_token = require("../middleware/auth");
 
 const student_managementRoute = (app) => {
-  app.get("/api/v1/student/get/all", validate_token(), getAllStudents);
+  app.get("/api/v1/student/get/all", getAllStudents);
   app.get("/api/v1/student/get/:targetId", getStudentById);
-  app.post("/api/v1/student/create", validate_token(), createStudent);
-  app.put("/api/v1/student/update/:targetId", validate_token(), udpateStudent);
+  app.post("/api/v1/student/create", createStudent);
+  app.put("/api/v1/student/update/:targetId", udpateStudent);
   app.delete(
     "/api/v1/student/delete/:targetId",
     validate_token(),
