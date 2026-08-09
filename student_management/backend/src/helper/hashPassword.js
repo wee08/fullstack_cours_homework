e6d4ce0d6d17bed4fff4a5b4a3290ada28f4fa6c
wee08 <manutnithyasean@gmail.com>
2026-08-09 10:46:56 +0700

    refactor : delete unuse var

--- student_management/backend/src/helper/hashPassword.js
+++ student_management/backend/src/helper/hashPassword.js
@@ -1,7 +1,5 @@
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
-const myPlaintextPassword = "s0/\/\P4$$w0rD";
-const someOtherPlaintextPassword = "not_bacon";
 
 async function hashPassword(user_password) {
   const password = await bcrypt.hash(user_password, saltRounds);
