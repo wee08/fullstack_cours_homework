e8adacb649f2e42a97df840b2cd0e0fbb6736a2d
wee08 <manutnithyasean@gmail.com>
2026-08-09 10:23:22 +0700

    feat:login route

--- student_management/backend/src/router/auth.route.js
+++ student_management/backend/src/router/auth.route.js
@@ -1,9 +1,10 @@
 const getAllUsers = require("../controller/auth/getAllUsers.controller");
 const signup = require("../controller/auth/signup.controller");
-
+const logIn = require("../controller/auth/logIn.controller");
 const authRoute = (app) => {
   app.get("/api/v1/auth/get/all", getAllUsers);
-  app.post("/api/vq/auth/signup", signup);
+  app.get("/api/v1/auth/login", logIn);
+  app.post("/api/v1/auth/signup", signup);
 };
 
 module.exports = authRoute;
