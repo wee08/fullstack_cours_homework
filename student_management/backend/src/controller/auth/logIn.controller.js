e479b9461345702d97df6aa72e23283a967e7e8c
wee08 <manutnithyasean@gmail.com>
2026-08-09 10:22:15 +0700

    feat: login controller

---
+++ student_management/backend/src/controller/auth/logIn.controller.js
@@ -0,0 +1,39 @@
+const { authDB } = require("../../config/config");
+const { checkEmail } = require("../../helper/validate");
+const fetchAllData = require("../../helper/fetchAllData");
+
+const logIn = async (req, res) => {
+  try {
+    const { log_email, log_password } = req.body;
+    const index = await checkEmail(log_email);
+
+    if (index == -1) {
+      return res.status(404).send({
+        status: false,
+        message: "this account doesn't exist, singup instead!",
+      });
+    }
+
+    const users = await fetchAllData("auths");
+
+    const user_name = users[index].user_name;
+    const email = users[index].email;
+    const password = users[index].password;
+    const phone = users[index].phone;
+
+    res.send({
+      user_name,
+      email,
+      password,
+      phone,
+    });
+  } catch (error) {
+    const content = error.message;
+    res.status(500).send({
+      status: false,
+      message: content,
+    });
+  }
+};
+
+module.exports = logIn;
