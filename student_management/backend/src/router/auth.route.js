89cf71c334ca65265fb6df7b19543977f8696e1f
wee08 <manutnithyasean@gmail.com>
2026-08-11 19:56:52 +0700

    fix:change route name

--- student_management/backend/src/router/auth.route.js
+++ student_management/backend/src/router/auth.route.js
@@ -8,7 +8,7 @@ const authRoute = (app) => {
   app.post("/api/v1/auth/login", logIn);
   app.post("/api/v1/auth/signup", signup);
 
-  app.post("/api/v1/auth/signup/verify", validateVerifyCode);
+  app.post("/api/v1/auth/otp/verify", validateVerifyCode);
   app.post("/api/v1/auth/send-otp", sendOTP);
   app.post("/api/v1/auth/reset-password", resetPassword);
 };
