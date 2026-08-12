fc4454fb0b50a631ca5e9a6e781d3fc07f5a5fee
wee08 <manutnithyasean@gmail.com>
2026-08-12 10:39:21 +0700

    fix: typo

--- student_management/backend/src/config/config.js
+++ student_management/backend/src/config/config.js
@@ -3,15 +3,15 @@ const path = require("path");
 const dotenv = require("dotenv");
 dotenv.config({ path: path.resolve(__dirname, "../../../.env") });
 
-const STUDNET_DB_HOST = process.env.STUDNET_DB_HOST;
-const STUDNET_DB_USER = process.env.STUDNET_DB_USER;
+const STUDENT_DB_HOST = process.env.STUDENT_DB_HOST;
+const STUDENT_DB_USER = process.env.STUDENT_DB_USER;
 
 const AUTH_DB_HOST = process.env.AUTH_DB_HOST;
 const AUTH_DB_USER = process.env.AUTH_DB_USER;
 
 const studentDB = mysql.createPool({
-  host: STUDNET_DB_HOST,
-  user: STUDNET_DB_USER,
+  host: STUDENT_DB_HOST,
+  user: STUDENT_DB_USER,
   password: "",
   database: "student_management",
 });
