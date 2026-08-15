fb71d38b53883f65fd20ef4590803ae4b5d2c2bf
wee08 <manutnithy08@gmail.com>
2026-08-15 12:22:49 +0700

    update: resete password controller

--- student_management/backend/src/controller/auth/resetPassword.controller.js
+++ student_management/backend/src/controller/auth/resetPassword.controller.js
@@ -3,17 +3,37 @@ const {
   getPendingCode,
   removePendingCode,
 } = require("../../global/storePendingCode");
+const Auths = require("../../models/Auths");
+
 const { hashPassword } = require("../../helper/hashPassword");
 const resetPassword = async (req, res) => {
   try {
     const { email, newPassword } = req.body;
+
+    const auth = await Auths.findOne({
+      where: { email },
+    });
+
     const pending = getPendingCode(email);
 
+    if (!auth) {
+      return res.status(404).send({
+        error: "Invalid user",
+      });
+    }
     const hashedPassword = await hashPassword(newPassword);
-    const sql = `
-        UPDATE auths SET password=? WHERE email=?
-    `;
-    await authDB.query(sql, [hashedPassword, email]);
+    // const sql = `
+    // UPDATE auths SET password=? WHERE email=?
+    // `;
+
+    const content = {
+      email,
+      password: hashedPassword,
+    };
+
+    const result = await auth.update(content);
+
+    // await authDB.query(sql, [hashedPassword, email]);
     return res.send({
       status: true,
       message: "New password is updated!",
