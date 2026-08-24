4b8b51d5f00c331b8f54b4c2e8cbd3619e834f60
wee08 <manutnithy08@gmail.com>
2026-08-24 10:46:23 +0700

    route for qr payment

--- student_management/backend/src/router/cardpayway.route.js
+++ student_management/backend/src/router/cardpayway.route.js
@@ -2,13 +2,16 @@ const {
   createCheckoutSession,
   paymentSuccess,
   generateKHQR,
+  verifyKHQR,
 } = require("../controller/payment/cardpayway.controller");
 
 const cardpaywayRoute = (app) => {
   app.post("/api/v1/cardpayway/stripe", createCheckoutSession);
   app.post("/api/v1/cardpayway/Success", paymentSuccess);
 
-  app.post("/api/v1/cardpayway/khqr", generateKHQR);
+  //k KHQR Route
+  app.post("/api/v1/cardpayway/verify", verifyKHQR);
+  app.post("/api/v1/cardpayway/generate-khqr", generateKHQR);
 };
 
 module.exports = cardpaywayRoute;
