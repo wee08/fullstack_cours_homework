769a62201e486de9ba836c031c31d7ddb33967de
wee08 <manutnithy08@gmail.com>
2026-08-25 19:04:54 +0700

    add UI to app

--- student_management/frontend/src/App.jsx
+++ student_management/frontend/src/App.jsx
@@ -1,8 +1,8 @@
-import Navbar from "./components/Navbar/Navbar.jsx";
+import UserInterface from "./components/ui/userInterface";
 const App = () => {
   return (
     <>
-      <Navbar />
+      <UserInterface />
     </>
   );
 };
