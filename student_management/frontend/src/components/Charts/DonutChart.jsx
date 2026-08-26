1907c2172045b391b2367b9b054e6ffbd69d3ebd
wee08 <manutnithyasean@gmail.com>
2026-08-26 15:18:30 +0700

    render donut chart

---
+++ student_management/frontend/src/components/Charts/DonutChart.jsx
@@ -0,0 +1,27 @@
+import { useEffect } from "react";
+import renderDonutChart from "./JS/renderDonutChart";
+const DonutChart = () => {
+  useEffect(() => {
+    renderDonutChart();
+  }, []);
+  return (
+    <div className="card chart-card chart-card--donut">
+      <div className="card-head">
+        <h2 className="card-title">Class-wise distribution</h2>
+      </div>
+      <div className="donut-wrap">
+        <svg
+          viewBox="0 0 160 160"
+          className="donut-chart"
+          id="donutChart"></svg>
+        <div className="donut-center">
+          <span className="donut-center-value">1,248</span>
+          <span className="donut-center-label">Students</span>
+        </div>
+      </div>
+      <ul className="donut-legend" id="donutLegend"></ul>
+    </div>
+  );
+};
+
+export default DonutChart;
