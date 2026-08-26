b9cdf11b1c8df8f9ffca3ee3fd4a057e4bfae8e8
wee08 <manutnithyasean@gmail.com>
2026-08-27 00:08:58 +0700

    Add reusable field validation helper

---
+++ student_management/frontend/src/helper/validateField.js
@@ -0,0 +1,10 @@
+import { setFieldState } from "../components/Modal/setFeildState";
+import { shake } from "./shake";
+
+export function validateField(fieldEl, validatorFn) {
+  const input = fieldEl.querySelector("input, textarea");
+  const ok = validatorFn(input.value);
+  setFieldState(fieldEl, ok ? "valid" : "invalid");
+  if (!ok) shake(input);
+  return ok;
+}
