cad91d66faddf71dddb9f489890cce9761704ef3
wee08 <manutnithyasean@gmail.com>
2026-08-26 15:19:24 +0700

    contain line and donut chart

---
+++ student_management/frontend/src/components/Charts/Index.jsx
@@ -0,0 +1,12 @@
+import LineChart from "./LineChart";
+import DonutChart from "./DonutChart";
+const Index = () => {
+  return (
+    <section className="chart-row">
+      <LineChart />
+      <DonutChart />
+    </section>
+  );
+};
+
+export default Index;
