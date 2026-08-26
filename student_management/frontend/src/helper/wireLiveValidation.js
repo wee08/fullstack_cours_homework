3f351872803fbfab9368a7ced4ecae35580b510d
wee08 <manutnithyasean@gmail.com>
2026-08-27 00:08:58 +0700

    Clean up live validation event listeners

---
+++ student_management/frontend/src/helper/wireLiveValidation.js
@@ -0,0 +1,18 @@
+import { validateField } from "./validateField";
+import { setFieldState } from "../components/Modal/setFeildState";
+export function wireLiveValidation(fieldEl, validatorFn) {
+  const input = fieldEl.querySelector("input, textarea");
+  const validateOnBlur = () => validateField(fieldEl, validatorFn);
+  const validateOnInput = () => {
+    if (fieldEl.classList.contains("is-invalid") && validatorFn(input.value))
+      setFieldState(fieldEl, "valid");
+  };
+
+  input.addEventListener("blur", validateOnBlur);
+  input.addEventListener("input", validateOnInput);
+
+  return () => {
+    input.removeEventListener("blur", validateOnBlur);
+    input.removeEventListener("input", validateOnInput);
+  };
+}
