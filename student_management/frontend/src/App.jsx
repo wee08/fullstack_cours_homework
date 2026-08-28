db53baacff73ca551f0e7c7ebc1561ffad2637e6
wee08 <manutnithyasean@gmail.com>
2026-08-28 12:14:53 +0700

    implement browser router

--- student_management/frontend/src/App.jsx
+++ student_management/frontend/src/App.jsx
@@ -1,10 +1,20 @@
-import UserInterface from "./ui/UserInterface";
-const App = () => {
-  return (
-    <>
-      <UserInterface />
-    </>
-  );
-};
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/Home/HomePage.jsx";
+import AllStudents from "./pages/AllStudnents/AllStudents.jsx";
+import Navbar from "./components/Navbar/Navbar.jsx";
+
+const App = () => (
+  <BrowserRouter>
+    <div className="app-shell">
+      <div className="sidebar-overlay" id="sidebarOverlay" />
+      <Navbar />
+
+      <Routes>
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/allstudents" element={<AllStudents />} />
+      </Routes>
+    </div>
+  </BrowserRouter>
+);
 
 export default App;
