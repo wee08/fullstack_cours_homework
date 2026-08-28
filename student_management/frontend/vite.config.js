3df1dc5586759322614b280e7fe43c6283f3f902
wee08 <manutnithyasean@gmail.com>
2026-08-28 12:14:38 +0700

    add config to project for better import organize

--- student_management/frontend/vite.config.js
+++ student_management/frontend/vite.config.js
@@ -1,8 +1,12 @@
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: { "@": path.resolve(__dirname, "src") },
+  },
 });
