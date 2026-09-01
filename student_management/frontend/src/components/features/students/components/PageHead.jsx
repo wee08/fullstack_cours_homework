30d9c20d11378775a8ad8d787a49771c159a5c1e
wee08 <manutnithyasean@gmail.com>
2026-09-01 11:52:49 +0700

    interact to Home

--- student_management/frontend/src/components/features/students/components/PageHead.jsx
+++ student_management/frontend/src/components/features/students/components/PageHead.jsx
@@ -1,17 +1,22 @@
 import { ChevronRight, Plus } from "lucide-react";
-
+import { useStudent } from "../context/StudentContext";
+import { Link } from "react-router-dom";
 const PageHead = () => {
+  const { openAddStudent } = useStudent();
   return (
     <div className="page-head">
       <div>
         <h1 className="page-title">Students List</h1>
         <p className="breadcrumb">
-          <span>Home</span>
+          <Link to="/">Home</Link>
           <ChevronRight />
           <span className="is-current">Students</span>
         </p>
       </div>
-      <button className="btn btn-primary" id="addStudentBtn">
+      <button
+        className="btn btn-primary"
+        id="addStudentBtn"
+        onClick={() => openAddStudent()}>
         <Plus />
         <span>Add Students</span>
       </button>
